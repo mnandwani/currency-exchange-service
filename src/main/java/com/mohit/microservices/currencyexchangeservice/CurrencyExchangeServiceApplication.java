6758fe086a2de3e4ccbc060e3069508c2feed742
VSPL\mnandwani <mohit.nandwani@vicsuper.com.au>
2021-08-09 12:34:41 +1000

    first commit

---
+++ src/main/java/com/mohit/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,16 @@
+package com.mohit.microservices.currencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+//@EntityScan("com.mohit.microservices.currencyexchangeservice")
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+}
